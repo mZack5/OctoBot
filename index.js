00fd111e51710c74be9d40a176f9f3eb6beb98cc
Zach <zaz1@pct.edu>
2017-03-26 21:26:15 -0400

    Added Command Handler

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const secrets = require("./secrets.json");
 
+var token = secrets.token;
+var prefix = secrets.prefix;
 
 
-bot.on("message", (message) => {
-    if(message.content == "!test") {
-       message.reply("    go fuck yourself");
-       // message.channel.sendMessage("pong");
+var commands = {
+  "commandname": {
+    func: function (message, args) {
+      message.channel.sendMessage("command/channel reply");
     }
 
-})
+  },
+  "hi": {
+    func: function (message, args) {
+      message.channel.sendMessage("Heyo You're Gayo!");
+    }
+  }
+
+};
 
 
-bot.login(secrets.token)
+bot.on("message", function (message) {
+  if (message.content.indexOf(prefix) === 0) {
+    var messageArray = message.content.slice(1).split(" ");
+    var cmd = commands[messageArray.shift()];
+    if (cmd !== undefined) {
+      cmd.func(message, messageArray);
+    } else {
+      message.channel.sendMessage("Unknown command my dude");
+    }
+  }
+});
+
+
+bot.login(token)
