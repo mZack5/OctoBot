258268b814b00144335e318257454cecde4d0fed
Zach <zaz1@pct.edu>
2017-03-28 23:09:51 -0400

    Added requests

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const secrets = require("./lib/secrets.json");
+const config = require("./lib/config.json");
 const commands = require("./lib/commands.js").commands;
+const request = require("request.js");
 
-var token = secrets.token;
-var prefix = secrets.prefix;
+var token = config.token;
+var prefix = config.prefix;
 
+
+bot.on("ready", () => {
+      bot.user.setGame("Dank Memes"); 
+});
 
 bot.on("message", function (message) {
   if (message.author.bot === false) {
     if (message.content.indexOf(prefix) === 0) {
-      var messageArray = message.content.slice(1).split(" ");
-      var cmd = commands[messageArray.shift()];
-      if (cmd !== undefined) {
-        cmd.func(message, messageArray);
-      } else {
+      var messageArguments = message.content.slice(1).split(" ");
+      var func = commands[messageArguments.shift()];
+      var command = message.content.slice(1).split(" ").shift();
+      if (func !== undefined) {
+        func.process(message, messageArguments, command);
+      } else if (command.length > 0) {
         message.channel.sendMessage("Unknown command! Type !Commands for a list of commands!");
       }
     }
