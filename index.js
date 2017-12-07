53436c71028896f744cdf2a2fe84929a29b0ad0b
Zachary <zach@Mz7.zodkoy.me>
2017-12-06 23:32:47 -0500

    added toLowerCase and sendMessage -> send

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- "use strict";
+"use strict";
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./lib/config.json");
@@ -9,20 +9,20 @@ let token = config.discord_token;
 let prefix = config.prefix;
 
 bot.on("ready", () => {
-      let game = config.game;
-      bot.user.setGame(game); 
+  let game = config.game;
+  bot.user.setGame(game);
 });
 
 bot.on("message", function (message) {
   if (message.author.bot === false) {
     if (message.content.indexOf(prefix) === 0) {
-      let messageArguments = message.content.slice(1).split(" ");
+      let messageArguments = message.cleanContent.slice(1).split(" ");
       let func = commands[messageArguments.shift()];
-      let command = message.content.slice(1).split(" ").shift();
+      let command = message.cleanContent.slice(1).split(" ").shift().toLowerCase();
       if (func !== undefined) {
         func.process(message, messageArguments, command);
       } else if (command.length > 0) {
-        message.channel.sendMessage("Unknown command! Type !Commands for a list of commands!");
+        message.channel.send('Unknown command! Type ' + prefix + 'Commands for a list of commands!');
       }
     }
   }
