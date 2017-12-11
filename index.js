50787abb3ec315f2f8c8bdb2015f08d2ec0e1b07
Zachary <zach@Mz7.zodkoy.me>
2017-12-11 18:15:49 -0500

    prefix commands works with prefixes > 1

--- index.js
+++ index.js
@@ -9,20 +9,21 @@ let token = config.discord_token;
 bot.prefix = config.prefix;
 
 bot.on("ready", function botReady() {
+  console.log('im ready');
   let game = config.game;
   bot.user.setGame(game);
 });
 
 bot.on("message", function messageRecived(message) {
   if (message.author.bot === false) {
-    if (message.content.indexOf(bot.prefix) === 0) {
-      let messageArguments = message.cleanContent.toLowerCase().slice(1).split(" ");
+    if (message.content.startsWith(bot.prefix)) {
+      let messageArguments = message.cleanContent.toLowerCase().slice(bot.prefix.length).split(" ");
       let func = commands[messageArguments.shift()];
-      let command = message.cleanContent.slice(1).split(" ").shift().toLowerCase();
+      let command = message.cleanContent.slice(bot.prefix.length).split(" ").shift().toLowerCase();
       if (func !== undefined) {
         func.process(message, messageArguments, command, bot);
       } else if (command.length > 0) {
-        message.channel.send('Unknown command! Type ' + prefix + 'Commands for a list of commands!');
+        message.channel.send('Unknown command! Type ' + bot.prefix + 'Commands for a list of commands!');
       }
     }
   }
