7897e60f9de14954c07904f6889e8dd3c7d38824
Zach <zaz1@pct.edu>
2017-12-17 14:44:41 -0500

    did stuff, removed debug lines and fixed spam command.

--- index.js
+++ index.js
@@ -16,20 +16,26 @@ bot.on("ready", function botReady() {
 
 bot.on("message", function messageRecived(message) {
   if (message.author.bot === false) {
-    let messageArguments = message.cleanContent.toLowerCase().slice(bot.prefix.length).split(" ");
+    let messageArguments = message.content.toLowerCase().slice(bot.prefix.length).split(" ");
     let func = commands[messageArguments.shift()];
-    let command = message.cleanContent.slice(bot.prefix.length).split(" ").shift().toLowerCase();
+    let command = message.content.slice(bot.prefix.length).split(" ").shift().toLowerCase();
     if (message.channel.type !== 'text') return message.channel.send('fuck outta my DMs boi');
     if (message.content.startsWith(bot.prefix)) {
       if (func !== undefined) {
+        /**
+         console.clear();
+         console.log('message.clean:     ' + message.cleanContent);
+         console.log('message command:   ' + command);
+         console.log('message args:      ' + messageArguments);
+         */
         func.process(message, messageArguments, command, bot);
       } else if (command == 'commands') {
         message.channel.send(Object.getOwnPropertyNames(commands));
       } else if (command.length > 0) {
         message.channel.send(`Unknown command! Type ${bot.prefix}commands for a list of commands`);
       }
-        } else if (command == 'prefix') {
-     // message.channel.send(`My prefix is currently ${bot.prefix}`);
+    } else if (command == 'prefix') {
+      message.channel.send(`My prefix is currently ${bot.prefix}`);
     }
   }
 });
