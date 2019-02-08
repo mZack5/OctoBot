d0aeace2f78b846bc436f6b2b360eb6bf4fcc808
name <Email>
2019-02-08 18:42:48 -0500

    Removed music bot, moved things around, version bumped everything

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const path = require("path");
 const express = require('express');
 const app = express();
 
-const Music = require('./lib/src/discord-music');
 const fs = require('fs');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 let config = JSON.parse(fs.readFileSync('./config.json'));
 const http = require("http");
@@ -29,7 +29,7 @@ fs.readdir('./lib/', (err, files) => {
     console.log('somethings wrong bud');
     return;
   } else console.log(`loading ${command_files.length} commands`);
-
+  
   command_files.forEach((f, i) => {
     let props = require(`./lib/${f}`);
     bot.commands.set(props.help.name, props);
@@ -41,45 +41,63 @@ bot.on("ready", function botReady() {
   console.log('im ready');
   // This should send a call to /lib/game.js
   // this shouldnt be its own call. 
-
+  // but im lazy
+  
   bot.user.setActivity(config.game, {
     url: config.url,
     type: config.game_state
-  });
+    
+  }); 
 });
 
 bot.on("message", function messageRecived(message) {
   if (message.author.bot === true) return;
   if (message.channel.type !== 'text' &&
-    message.author.id !== config.bot_owner) {
+  message.author.id !== config.bot_owner) {
     return message.channel.send('fuck outta my DMs boi');
   }
   let messageArguments = message.content.slice(bot.prefix.length).split(" ");
   messageArguments.shift();
   let command = message.content.slice(bot.prefix.length).split(" ").shift();
   let func = bot.commands.get(command);
-
+  
   if (message.content.startsWith(bot.prefix)) {
     if (func) {
       func.run(message, messageArguments, command, bot);
     }
     //else if (command == 'commands') {
-    //   let cmd_names = [];
-    //   bot.commands.forEach((objects, names) => {
-    //     cmd_names.push(names);
-    //   });
-    //   message.channel.send(cmd_names);
-    // } 
-    else if (config.unknown_command_message == "true") {
-      message.channel.send("Unknown command!")
-    }
-  } else if (command == 'prefix') {
-    message.channel.send(`My prefix is currently ${bot.prefix}`);
-  }
-});
+      //   let cmd_names = [];
+      //   bot.commands.forEach((objects, names) => {
+        //     cmd_names.push(names);
+        //   });
+        //   message.channel.send(cmd_names);
+        // } 
+        else if (config.unknown_command_message == "true") {
+          message.channel.send("Unknown command!")
+        }
+      } else if (command == 'prefix') {
+        message.channel.send(`My prefix is currently ${bot.prefix}`);
+      }
+    });
+    
+    setInterval(() => {
+      http.get('http://fuck-zach.herokuapp.com');
+    }, 900000);
+    
+    bot.login(process.env.discordtoken);
+    
+    
+    
+    
+    
+    
+    
+    // google disabled my api key so uh, lets just disable the music bot
 
-const music = new Music(bot, {
-  youtubeKey: process.env.youtubetoken,
+    //const Music = require('./lib/src/discord-music');
+    /*
+    const music = new Music(bot, {
+      youtubeKey: process.env.youtubetoken,
   botOwner: config.bot_owner,
   prefix: bot.prefix,
   global: false,         //TODO: Change well bot is running!
@@ -94,11 +112,6 @@ const music = new Music(bot, {
   ownerOverMember: true,
   enableQueueStat: true,
   anyoneCanAdjust: false,
-  logging: false, 
+  logging: false,
 });
-
-setInterval(() => {
-  http.get('http://fuck-zach.herokuapp.com');
-}, 900000);
-
-bot.login(process.env.discordtoken);
+*/
