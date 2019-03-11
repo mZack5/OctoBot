cc21362c1911ff7e45ff5924e1b567f891412b25
name <Email>
2019-03-10 22:06:47 -0400

    Refactored index.js, and optimized the command handler

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 'use strict';
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const path = require('path');
-const express = require('express');
+
 const schedule = require('node-schedule');
+const fs = require('fs');
+const request = require('request');
+const tiktokpinger = require('./lib/src/tiktokPinger');
+
+const express = require('express');
+const path = require('path');
 const app = express();
 
-const fs = require('fs');
 require('dotenv').config();
-const tiktokpinger = require('./lib/src/tiktokPinger');
 const port = process.env.PORT || 5000;
 let config = JSON.parse(fs.readFileSync('./config.json'));
-const http = require('http');
-
 bot.prefix = config.prefix;
-bot.commands = new Discord.Collection;
+
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
@@ -24,18 +25,15 @@ app.listen(port, () => {
   console.log('Our app is running on http://localhost:' + port);
 });
 
+bot.commands = new Discord.Collection;
 fs.readdir('./lib/', (err, files) => {
   if (err) console.error(err);
-  let command_files = files.filter(f => f.split('.').pop() === 'js');
-  if (command_files.length <= 0) {
-    console.log('somethings wrong bud');
-    return;
-  } else console.log(`loading ${command_files.length} commands`);
-
-  command_files.forEach((f, i) => {
-    let props = require(`./lib/${f}`);
+  // this line only selects .js files, and adds them to command_files
+  let command_files = files.filter(f => f.includes('.js'));
+  for (let cmd of command_files) {
+    let props = require(`./lib/${cmd}`)
     bot.commands.set(props.help.name, props);
-  });
+  }
 });
 
 
@@ -87,19 +85,8 @@ bot.on('error', (error) => {
 
 // this stops heroku from disabling my dynamo from no traffic
 setInterval(() => {
-  http.get('http://fuck-zach.herokuapp.com');
+  request.get('http://fuck-zach.herokuapp.com');
 }, 900000);
-
-
-// this is to check if X tiktoker has uploaded, and send a discord
-// message as an alert
-
-// this function should:
-/**
- * have an init function in bot.on(ready) which updates 
- * when booting, so reboots dont always invoke an alert
- * 
- */
 
 let job = schedule.scheduleJob({
   minute: 30
@@ -120,8 +107,6 @@ let job3 = schedule.scheduleJob({
   let teeth = JSON.parse(fs.readFileSync('./lib/src/teeth.json'));
   bot.channels.get('381974359843012613').send(`<@139465047704469504> ${teeth.brush[Math.floor(Math.random() * Math.floor(teeth.brush.length))]}`);
 });
-
-
 
 
 bot.login(process.env.discordtoken);
