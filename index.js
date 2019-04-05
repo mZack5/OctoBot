be75600116a91b2e3fa36c2bab80cc791872d408
monsterzack5 <monsterzack5@gmail.com>
2019-04-05 01:53:49 -0400

    Added system to upload/download files from discord to store/load any given file

--- index.js
+++ index.js
@@ -7,117 +7,125 @@ const fs = require('fs');
 const request = require('request-promise-native');
 const tiktokpinger = require('./lib/tools/tiktokPinger');
 const checkIfLive = require('./lib/tools/checkIfLive');
-const configLoader = require('./lib/tools/configLoader');
+const fileLoader = require('./lib/tools/fileLoader');
 
 const express = require('express');
 const path = require('path');
 const app = express();
 
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5010;
 
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname + '/views/index.html'));
+    response.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.listen(port, () => {
-  console.log('Program Starting');
+    console.log('Program Starting');
 });
 
 bot.commands = new Discord.Collection;
 fs.readdir('./lib/', (err, files) => {
-  if (err) console.error(err);
-  // this line only selects .js files, and adds them to command_files
-  let command_files = files.filter(f => f.includes('.js'));
-  for (let cmd of command_files) {
-    let props = require(`./lib/${cmd}`);
-    try {
-      bot.commands.set(props.help.name, props);
-    } catch (error) {
-      console.log(`Error adding commands, file doesnt have name or help properties`);
-      process.exit(1);
+    if (err) console.error(err);
+    // this line only selects .js files, and adds them to command_files
+    let command_files = files.filter(f => f.includes('.js'));
+    for (let cmd of command_files) {
+        let props = require(`./lib/${cmd}`);
+        try {
+            bot.commands.set(props.help.name, props);
+        } catch (error) {
+            console.log(`Error adding commands, file doesnt have name or help properties`);
+            process.exit(1);
+        }
     }
-  }
 });
 
-bot.on('ready', function botReady() {
-  console.log('Discord Client ready');
+bot.on('ready', async () => {
+    console.log('Discord Client ready');
+    // this is to load tiktokers.json and config.json on reboot
 
-  // this is to update tiktokers.json and config.json on reboot
-  // using info from a discord message  
-  tiktokpinger.importTikTokers(bot);
-  configLoader.importConfig(bot).then(() => {
+    try {
+        await fileLoader.importFile(bot, 'config.json');
+        await fileLoader.importFile(bot, 'tiktokers.json');
+    } catch (error) {
+        console.log(`Fatal Error! \n${error}`);
+        process.exit();
+    }
+
     let config = JSON.parse(fs.readFileSync('./config.json'));
     bot.prefix = config.prefix;
     bot.unknown_command_message = config.unknown_command_message;
 
     bot.user.setActivity(config.game, {
-      url: config.game_url,
-      type: config.game_state
+        url: config.game_url,
+        type: config.game_state
     });
-  });
 });
 
-bot.on('message', function messageRecived(message) {
-  if (message.author.bot === true) return;
-  if (message.channel.type !== 'text' &&
-    message.author.id !== config.bot_owner) {
-    return message.channel.send('fuck outta my DMs boi');
-  }
-  let messageArguments = message.content.slice(bot.prefix.length).split(' ');
-  messageArguments.shift();
-  let command = message.content.slice(bot.prefix.length).split(' ').shift();
-  let cmdfunction = bot.commands.get(command);
-
-  if (message.content.startsWith(bot.prefix)) {
-    if (cmdfunction) {
-      cmdfunction.run(message, messageArguments, command, bot);
-    } else if (bot.unknown_command_message == 'true') {
-      message.channel.send('Unknown command!')
+bot.on('message', async (message) => {
+    if (message.author.bot === true) return;
+    if (message.channel.type !== 'text' &&
+        message.author.id !== config.bot_owner) {
+        return message.channel.send('fuck outta my DMs boi');
     }
-  } else if (command == 'prefix') {
-    // this words because when checking for a function
-    // the first letter is removed, meaning things 
-    // like aprefix or @prefix also work
+    let messageArguments = message.content.slice(bot.prefix.length).split(' ');
+    messageArguments.shift();
+    let command = message.content.slice(bot.prefix.length).split(' ').shift();
+    let cmdfunction = bot.commands.get(command);
 
-    // TODO: make this not work based on len of prefix, 
-    // so !prefix actually works likes its supposed too
-    message.channel.send(`My prefix is currently ${bot.prefix}`);
-  }
+    if (message.content.startsWith(bot.prefix)) {
+        if (cmdfunction) {
+            cmdfunction.run(message, messageArguments, command, bot);
+        } else if (bot.unknown_command_message == 'true') {
+            message.channel.send('Unknown command!')
+        }
+    } else if (command == 'prefix') {
+        // this words because when checking for a function
+        // the first letter is removed, meaning things 
+        // like aprefix or @prefix also work
+
+        // TODO: make this not work based on len of prefix, 
+        // so !prefix actually works likes its supposed too
+        message.channel.send(`My prefix is currently ${bot.prefix}`);
+    }
 });
 
-bot.on('error', (error) => {
-  setTimeout(() => {
-    tiktokpinger.checkIfNewVideos();
-  }, 45000)
-  console.error(`Something went wrong... \n\r ${JSON.stringify(error, null, 2)}}`);
+bot.on('error', async (error) => {
+    setTimeout(() => {
+        tiktokpinger.checkIfNewVideos();
+    }, 45000)
+    console.error(`Something went wrong... \n\r ${JSON.stringify(error, null, 2)}}`);
 });
 
+process.on('SIGINT', () => {
+    console.log("aught interrupt signal");
+    process.exit();
+});
 
 // this stops heroku from disabling my dynamo from no traffic
 setInterval(() => {
-  request({
-    uri: 'http://fuck-zach.herokuapp.com',
-  }).catch((err) => {
-    console.log('this isnt supposed to happen');
-  });
+    request({
+        uri: 'http://fuck-zach.herokuapp.com',
+    }).catch((err) => {
+        console.log('this isnt supposed to happen');
+    });
 }, 900000);
 
 // auto updater for new posts, checks 2 times an hour
 cron.schedule('30,58 * * * *', function () {
-  tiktokpinger.checkIfNewVideos(bot);
+    tiktokpinger.checkIfNewVideos(bot);
 });
 
 // auto updater for if someone is live, checks every 5 minutes
 cron.schedule('*/5 * * * *', function () {
-  checkIfLive.checkIfLive(bot);
+    //checkIfLive.checkIfLive(bot);
 });
 // this is to tell my friend to brush his teeth
 cron.schedule('5 18 * * *', function () {
-  let teeth = JSON.parse(fs.readFileSync('./lib/tools/teeth.json'));
-  bot.channels.get('381974359843012613').send(`<@139465047704469504> ${teeth.brush[Math.floor(Math.random() * Math.floor(teeth.brush.length))]}`);
+    let teeth = JSON.parse(fs.readFileSync('./lib/tools/teeth.json'));
+    bot.channels.get('381974359843012613').send(`<@139465047704469504> ${teeth.brush[Math.floor(Math.random() * Math.floor(teeth.brush.length))]}`);
 });
 
 
