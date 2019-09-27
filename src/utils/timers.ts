f05fc83575618056e10e55db424ff8751be66d54
monsterzack5 <monsterzack5@gmail.com>
2019-09-27 07:18:07 -0400

    No sleeping on the job

--- src/utils/timers.ts
+++ src/utils/timers.ts
@@ -16,7 +16,7 @@ export function startTimers(): void {
    if (process.env.NODE_ENV === 'production') {
       // ping our dyno every 15 minutes so heroku doesnt murder it
       herokuPing = setInterval((): void => {
-         http.get('http://catbitchbot.herokuapp.com');
+         http.get('https://botty-boi.herokuapp.com/');
       }, 900000);
    }
 }
